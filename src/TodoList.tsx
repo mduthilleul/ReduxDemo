3d26b3de740e8ec7c97e931a15ccf50fb2b1e783
Michaël Duthilleul <michael.duthilleul@matera.eu>
2020-12-08 00:47:55 +0100

    Async version

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,20 +1,31 @@
 import CheckBox from "@react-native-community/checkbox"
 import { useNavigation } from "@react-navigation/native"
-import React from "react"
-import { Button, FlatList, Text, View } from "react-native"
-import { useSelector } from "react-redux"
-import { RootState } from "./store"
+import React, { useEffect } from "react"
+import { ActivityIndicator, Button, FlatList, Text, View } from "react-native"
+import { useDispatch } from "react-redux"
+
+import { fetchTodos, toggleTodo, useTodos } from "./todoReducer"
 
 export default () => {
-    const todos = useSelector((state: RootState) => state.todo.todos)
+    const {todos, loading} = useTodos()
+    const dispatch = useDispatch()
+
     const {navigate} = useNavigation()
+
+    useEffect(() => {
+        dispatch(fetchTodos())
+    }, [])
+
     return (
         <View style={{flex: 1, padding: 20}}>
+            {loading && <ActivityIndicator />}
             <FlatList style={{flex: 1}} data={todos} renderItem={
-                ({item: {label, done}}) => (
+                ({item: {label, done, id}}) => (
                     <View style={{paddingTop: 10, flex: 1, flexDirection: "row", alignItems: "center"}}>
                         <Text style={{flex: 1}}>{label}</Text>
-                        <CheckBox value={done} />
+                        <CheckBox value={done} onValueChange={() => {
+                            dispatch(toggleTodo(id))
+                        }} />
                     </View>
                 )
             } keyExtractor={({id}) => String(id)} />
--- src/todoReducer.ts
+++ src/todoReducer.ts
@@ -1,31 +1,79 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSelector, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { useSelector } from "react-redux";
+import { RootState } from "./store";
 
-const TODOS = [
-    {id: 1, label: "Petit déjeuner", done: false },
-    {id: 2, label: "Présentation redux", done: false}
-]
+type Todo = {
+    id: number
+    label: string
+    done: boolean
+}
+
+
+export const fetchTodos = createAsyncThunk("todos/fetchAll", () => new Promise<Todo[]>((resolve) => {
+    setTimeout(() => {
+        resolve([
+            {id: 1, label: "Petit déjeuner", done: false },
+            {id: 2, label: "Présentation redux", done: false}
+        ])
+    }, 1000)
+}))
+
+export const addTodo = createAsyncThunk("todos/add", (label: string, {getState}) => new Promise<Todo>((resolve, reject) => {
+    setTimeout(() => {
+        if(label.length < 3) {
+            reject()
+        } else {
+            resolve({
+                id: (getState() as any).todo.todos.length + 1,
+                label: "plop",
+                done: false,
+            })
+        }
+    }, 1000)
+}))
 
 const todoSlice = createSlice({
     name: "todo",
     initialState: {
-        todos: TODOS
+        loading: false,
+        todos: [] as Todo[],
     },
     reducers: {
-        addTodo: (state, action: PayloadAction<string>) => {
-            state.todos.push({
-                label: action.payload, done: false, id: state.todos.length + 1,
-            })
-        },
         toggleTodo: (state, action: PayloadAction<number>) => {
            const todo = state.todos.find(t => t.id === action.payload)
-           if(todo) {
+           if (todo) {
                todo.done = !todo?.done
            }
-        }
+        }, 
+    },
+    extraReducers: builder => {
+        builder.addCase(fetchTodos.pending, (state) => {
+            state.loading = true
+        })
+        builder.addCase(fetchTodos.fulfilled, (state, action) => {
+            state.todos = state.todos.concat(action.payload.filter(({id}) => !state.todos.find(t => t.id === id)))
+            state.loading = false
+        }),
+        builder.addCase(addTodo.pending, (state) => {
+            state.loading = true
+        }),
+        builder.addCase(addTodo.fulfilled, (state, action) => {
+            state.loading = false
+            state.todos.push(action.payload)
+        }),
+        builder.addCase(addTodo.rejected, (state) => {
+            state.loading = false
+        })
     }
 })
 
-
 export default todoSlice.reducer
 
-export const {addTodo} = todoSlice.actions
+export const {toggleTodo} = todoSlice.actions
+
+const selectTodos = createSelector((state: RootState) => ({
+    todos: [...state.todo.todos].sort((t1, t2) => t1.id - t2.id),
+    loading: state.todo.loading
+}), value => value)
+
+export const useTodos = () => useSelector(selectTodos)
